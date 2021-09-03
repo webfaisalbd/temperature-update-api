45aece427c8c193c55d7ab6d3b92a02a8ee8446d
faisalcsebd <faisal.cse.bd@gmail.com>
2021-09-03 21:55:25 +0600

    adding sunsrise and sunset features

--- js/app.js
+++ js/app.js
@@ -21,11 +21,49 @@ searchButton.addEventListener('click', function () {
 
 const display = data => {
     // calculation 
+    // console.log(data);
     const nameOfCity = data.name;
     const temp = (data.main.temp - 273.15).toPrecision(3);
+    const weatherStatus = data.weather[0].description;
 
-    // show result 
+    // show name of city , temperature and weather status 
     document.getElementById('show_city').innerText = nameOfCity;
     document.getElementById('show_temperature').innerText = temp;
-    console.log(temp);
+    document.getElementById('weather_status').innerText = weatherStatus;
+
+    // call function to convert sunset and sunrise
+    // taking let because after the line it changes value
+    let sunriseTime = convertUnixTimeToLocal(data.sys.sunrise);
+    let sunsetTime = convertUnixTimeToLocal(data.sys.sunset);
+    sunriseTime = sunriseTime.time12h
+    sunsetTime = sunsetTime.time12h
+
+    // show sunrise and sunset
+    document.getElementById('sunrise').innerText = sunriseTime;
+    document.getElementById('sunset').innerText = sunsetTime;
+    //set icon 
+    const url = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    const iconAdd = document.getElementById('icon');
+    iconAdd.setAttribute('src', url);
 }
+
+
+// convert unix time to local format
+const convertUnixTimeToLocal = unixTime => {
+    const milliSeconds = unixTime * 1000;
+    const humanDateFormat = new Date(milliSeconds);
+    const convertedTimeObject = {
+        fullDate: humanDateFormat.toLocaleString('en-US', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+        }),
+        time12h: humanDateFormat.toLocaleString('en-US', {
+            hour: 'numeric',
+            minute: 'numeric',
+            hour12: true,
+        }),
+    };
+    return convertedTimeObject;
+};
+
